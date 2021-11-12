d6c0575cc6acfe305ed85c6c0882787b75d19d96
Мещеряков Денис Владимирович <Meshcheryakov.DV@ruspace.digital>
2021-11-12 18:06:30 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=7e38fde0788b6840a4d358435022c898&units=metric"
-    https.get(url, function(response) {
-        console.log(response.statusCode);
-        response.on("data", function(data) {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<p>Weather is currently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in Moscow is " + temp + " degrees.</h1>");
-            res.write("<img src=" + imageURL + ">");
-            res.send();
-        })
-    });
-    //res.send("The server is up and running.");
+    res.sendFile(__dirname + "/index.html");
 });
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
 });
+
+app.post("/", function(req, res) {
+    console.log(req.body.cityName);
+})
+
+// const query = "Moscow";
+//     const apiKey = "7e38fde0788b6840a4d358435022c898";
+//     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=metric"
+//     https.get(url, function(response) {
+//         console.log(response.statusCode);
+//         response.on("data", function(data) {
+//             const weatherData = JSON.parse(data);
+//             const temp = weatherData.main.temp;
+//             const weatherDescription = weatherData.weather[0].description;
+//             const icon = weatherData.weather[0].icon;
+//             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+//             res.write("<p>Weather is currently " + weatherDescription + "</p>");
+//             res.write("<h1>The temperature in Moscow is " + temp + " degrees.</h1>");
+//             res.write("<img src=" + imageURL + ">");
+//             res.send();
+//         })
+//     });
+//     //res.send("The server is up and running.");
